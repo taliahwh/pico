53eea067be52061660759a6275d94aaef6fe16b5
Taliah Wharton <taliahwh23@yahoo.com>
2023-09-10 16:41:07 -0400

    multi led/button test

---
+++ DoubleSwitch.py
@@ -0,0 +1,62 @@
+import machine
+import utime
+
+"""
+red - 15
+yellow - 14 
+green - 13 
+blue - 12
+button - 11
+
+"""
+
+# setup I/O pins 
+red_led = machine.Pin(15, machine.Pin.OUT)
+yellow_led = machine.Pin(14, machine.Pin.OUT)
+green_led = machine.Pin(13, machine.Pin.OUT)
+blue_led = machine.Pin(12, machine.Pin.OUT)
+button = machine.Pin(11, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+counter = 0
+
+# loop reads button
+while True:
+  if button.value() == 1:
+    
+    counter = counter + 1
+    print(f'Counter: {counter}')
+
+    # reset counter
+    if counter == 4:
+      counter = 0
+
+
+    if counter == 1:
+      red_led.value(0)
+      yellow_led.value(0)
+      green_led.value(0)
+      blue_led.value(0)
+
+    elif counter == 2:
+       red_led.value(0)
+       yellow_led.value(1)
+       green_led.value(0)
+       blue_led.value(0)
+
+    elif counter == 3:
+       red_led.value(0)
+       yellow_led.value(0)
+       green_led.value(1)
+       blue_led.value(0)
+    
+    else:
+       red_led.value(0)
+       yellow_led.value(0)
+       green_led.value(0)
+       blue_led.value(1)
+
+    
+    utime.sleep(1)
+
+ 
+  
